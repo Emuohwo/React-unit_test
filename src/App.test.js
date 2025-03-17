8302f1e460f78f1e385f06d8ed4aa6f496b9518a
Emuohwo <warriisaac@outlook.com>
2025-03-17 09:09:43 +0100

    fix failing test

--- src/App.test.js
+++ src/App.test.js
@@ -6,8 +6,3 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-test('renders learn link', () => {
-  render(<App />);
-  const linkElement = screen.getByText("learn teact");
-  expect(linkElement).toBeInTheDocument();
-});
